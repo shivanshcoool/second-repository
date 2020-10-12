e045914b16810560ce9d8c08c968c88f064e289c
Shivansh063 <31778127+Shivansh063@users.noreply.github.com>
2020-10-13 01:27:03 +0530

    Update print_hello.java

--- print_hello.java
+++ print_hello.java
@@ -1,11 +1,5 @@
-class a
-{
-public static void main(String args[])
-{
-int a,b;
-  a=10;
-  b=20;.
-  int sum=0;
-  sum = a+b;
-  System.out.println("Sum : " + sum);
-}}
+class A{
+  public static void main(String args[]){
+    System.out.println("Hello World");
+  }
+}
